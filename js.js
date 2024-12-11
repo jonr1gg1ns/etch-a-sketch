e04083f6bf2adcb7cd31adb507f0aff688652719
jonr1gg1ns <jonathanriggins08@gmail.com>
2024-12-10 19:55:13 -0800

    finished, added the buttons to reset and make new grids, added css, might return to make better

--- js.js
+++ js.js
@@ -1,10 +1,10 @@
-const container16 = document.querySelector(".container16");
 
+const container16 = document.querySelector(".container16");
 function makeGrid(rows, columns) {
     container16.innerHTML = "";
 
     
-    const containerSize = Math.min(window.innerWidth, window.innerHeight) * 0.8; 
+    const containerSize = Math.min(window.innerWidth, window.innerHeight) * 0.7; 
     container16.style.width = `${containerSize}px`;
     container16.style.height = `${containerSize}px`;
 
@@ -18,9 +18,13 @@ function makeGrid(rows, columns) {
         cell.className = "cell";
         cell.style.width = `${cellSize}px`;
         cell.style.height = `${cellSize}px`;
+
+        cell.addEventListener("mouseover", () => {
+        cell.style.backgroundColor = "black";}) 
         container16.appendChild(cell);
     }
 }
+
 
 function grid() {
     makeGrid(16, 16);
@@ -29,6 +33,41 @@ function grid() {
 grid();
 
 window.addEventListener("resize", () => grid());
+const title = document.querySelector(".title")
+const button1 = document.createElement("button");
+button1.classList.add("button1")
+button1.textContent = "Choose number of squares";
+const body = document.querySelector(".body")
+body.appendChild(button1);
+document.body.prepend(button1);
+document.body.prepend(title);
+function buttonPress () {
+    let gridSize = parseInt(prompt("Choose grid size 1-100:"), 10);
+
+    if (isNaN(gridSize) || gridSize < 1 || gridSize > 100) {
+        alert("Numbers between 1-100 only please.");
+        return;
+    }
+
+    makeGrid(gridSize, gridSize);
+
+    
 
 
 
+}
+
+
+grid();
+
+button1.addEventListener("click", buttonPress);
+
+window.addEventListener("resize", () => grid())
+
+const button2 = document.createElement("button");
+button2.classList.add("button2");
+button2.textContent = "Reset";
+
+body.appendChild(button2);
+
+button2.addEventListener("click", () => makeGrid(16, 16))
